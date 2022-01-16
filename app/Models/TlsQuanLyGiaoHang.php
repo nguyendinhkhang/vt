9b3b5df867d21525f4c35ac95006398a537b67a9
TaiLS <tlskibbi@gmail.com>
2022-01-16 11:33:54 +0700

    update hen giao

--- app/Models/TlsQuanLyGiaoHang.php
+++ app/Models/TlsQuanLyGiaoHang.php
@@ -113,10 +113,13 @@ class TlsQuanLyGiaoHang extends Model
                     ->select('tls_quan_ly_giao_hangs.id_quan_ly_giao', 'tls_quan_ly_kho_hangs.ma_so_seri', 'tls_quan_ly_giao_hangs.dia_chi_giao_hang', 
                     'tls_quan_ly_giao_hangs.so_luong', 'tls_quan_ly_kho_hangs.gia_ban' ,'tls_quan_ly_giao_hangs.ngay_giao', 'tls_quan_ly_giao_hangs.trang_thai',
                     'tls_quan_ly_giao_hangs.user_giao_hang', 'tls_quan_ly_kho_hangs.kho_hang', 'tls_quan_ly_giao_hangs.ten_khach_hang', 'tls_quan_ly_giao_hangs.so_dien_thoai',
-                    'tls_quan_ly_giao_hangs.ghi_chu', 'users.name')
+                    'tls_quan_ly_giao_hangs.ghi_chu', 'users.name', 'tls_quan_ly_giao_hangs.latitude', 'tls_quan_ly_giao_hangs.longitude')
                     ->join('tls_quan_ly_kho_hangs', 'tls_quan_ly_kho_hangs.id_kho_hang', '=', 'tls_quan_ly_giao_hangs.id_kho_hang')
                     ->leftJoin('users', 'users.id', '=', 'tls_quan_ly_giao_hangs.user_giao_hang')
                     ->where('tls_quan_ly_giao_hangs.user_giao_hang', '=', $user_id)
+                    ->where('tls_quan_ly_giao_hangs.trang_thai = 1')
+                    ->orWhere('tls_quan_ly_giao_hangs.trang_thai = 2')
+                    ->orWhere('tls_quan_ly_giao_hangs.trang_thai = 4')
                     ->orderBy('id_quan_ly_giao', 'DESC')->get();
         return $result;
     }
